4343a74110d540ad298d3dd1b5dac459dae4d8e4
Miguel teheran <miguelteheran@gmail.com>
2020-03-26 17:23:04 -0500

    se sube proyecto base

---
+++ NetUniversityLinq/NetUniversityLinq/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetUniversityLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
