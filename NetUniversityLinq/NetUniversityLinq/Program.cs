73f9f9316a74d377a87b19e23728efd111ad56bf
Miguel teheran <miguelteheran@gmail.com>
2020-03-26 19:32:30 -0500

    Primer demo con LINQ

--- NetUniversityLinq/NetUniversityLinq/Program.cs
+++ NetUniversityLinq/NetUniversityLinq/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NetUniversityLinq
 {
@@ -6,7 +8,22 @@ namespace NetUniversityLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arrayInt = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<string> listString = new List<string>();
+            listString.Add("Maria");
+            listString.Add("Manuel");
+            listString.Add("Alberto");
+            listString.Add("Roberto");
+
+            //var arrayPar = from i in arrayInt
+            //               where i % 2 == 0
+            //               select i;
+            var arrayPar = arrayInt.Where(p => p % 2 == 0);
+
+            foreach (var item in arrayPar)
+            {
+                Console.WriteLine(item);
+            }
 
             Console.ReadLine();
         }
