8c470c248ff52be4de874b5bab90e25c40890e34
Miguel teheran <miguelteheran@gmail.com>
2020-03-31 19:52:36 -0500

    Where 1

--- NetUniversityLinq/NetUniversityLinq/Program.cs
+++ NetUniversityLinq/NetUniversityLinq/Program.cs
@@ -11,6 +11,25 @@ namespace NetUniversityLinq
             var cursos = Data.GetCursos();
             var estudiantes = Data.GetEstudiantes();
 
+            var cursosFiltrada = from c in cursos
+                                 where c.Activo == true
+                                 select c;
+            var estudiantesFiltrada = estudiantes.Where(p=> p.Activo == true);
+
+            Console.WriteLine(" --  CURSOS Filtrado -- ");
+            foreach (var item in cursosFiltrada)
+            {
+                Console.WriteLine($"{item.Codigo} - {item.Nombre}");
+            }
+            Console.WriteLine();
+            Console.WriteLine(" --  ESTUDIANTES Filtrada-- ");
+            foreach (var item in estudiantesFiltrada)
+            {
+                Console.WriteLine($"{item.Codigo} - {item.Nombre} {item.Apellido}");
+            }
+
+            Console.WriteLine();
+
             Console.WriteLine(" --  CURSOS -- ");
             foreach (var item in cursos)
             {
