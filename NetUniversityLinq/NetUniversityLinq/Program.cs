0edeee42b86e193d22a153fd940dfd6457ea1b27
Miguel teheran <miguelteheran@gmail.com>
2020-03-31 22:50:59 -0500

    Contains

--- NetUniversityLinq/NetUniversityLinq/Program.cs
+++ NetUniversityLinq/NetUniversityLinq/Program.cs
@@ -16,8 +16,8 @@ namespace NetUniversityLinq
                                  select c;
             var estudiantesFiltrada = estudiantes.Where((p,i) => p.Activo && i > 3);
 
-            var TodoslosEstudiantesActivos = estudiantes.Any(p => !p.Activo);
-            Console.WriteLine($"¿Estan todos los estudiantes activos? {(!TodoslosEstudiantesActivos).ToString()}");
+            var existeEstudianteSeleccionado = estudiantesFiltrada.Contains(estudiantes[5]);
+            Console.WriteLine($"¿El Estudiante seleccionado esta en la lista? {existeEstudianteSeleccionado.ToString()}");
 
             Console.WriteLine(" --  CURSOS Filtrado -- ");
             foreach (var item in cursosFiltrada)
